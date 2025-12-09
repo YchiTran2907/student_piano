bb950e2fabe6cc35a3c324b7d97f88ce9ed848f1
YChiTQ <ychitq@saisystem.vn>
2025-12-09 15:53:58 +0700

    Fix bug lần 3

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,21 +1,13 @@
-import { PrismaClient } from '@prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
-import { Pool } from 'pg'
+import { PrismaClient } from "@prisma/client"
 
-const globalForPrisma = global as unknown as {
-    prisma?: PrismaClient
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | undefined
 }
-
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-})
-
-const adapter = new PrismaPg(pool)
 
 export const prisma =
     globalForPrisma.prisma ??
     new PrismaClient({
-        adapter,
+        log: ['error'],
     })
 
 if (process.env.NODE_ENV !== 'production') {
